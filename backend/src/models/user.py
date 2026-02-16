0b3d6a78a31e09adb5e6ee70d0730cccfd066d7b
keteiru <vyazowneyr@gmail.com>
2026-02-17 01:29:54 +0300

    Настроил БД миграции

---
+++ backend/src/models/user.py
@@ -0,0 +1,17 @@
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from passlib.context import CryptContext
+
+Base = declarative_base()
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True)
+    username = Column(String, unique=True, index=True)
+    hashed_password = Column(String)
+
+    def verify_password(self, password: str):
+        return pwd_context.verify(password, self.hashed_password)
