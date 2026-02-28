bdbbf8263068edb8d14f1cbec111cb181666a79c
keteiru <vyazowneyr@gmail.com>
2026-02-28 23:55:27 +0300

    Пробуем так

--- backend/src/services/auth.py
+++ backend/src/services/auth.py
@@ -20,5 +20,4 @@ def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
         expire = datetime.utcnow() + timedelta(minutes=30)
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
-    return encoded_jwt
-    
+    return encoded_jwt    
