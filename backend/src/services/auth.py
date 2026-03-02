f73bc8f02cc4607398da669c4e8f0964a19b8afb
keteiru <vyazowneyr@gmail.com>
2026-03-03 01:06:10 +0300

    добваляем проверку

--- backend/src/services/auth.py
+++ backend/src/services/auth.py
@@ -4,7 +4,12 @@ from src.core.config import settings
 from datetime import timedelta, datetime
 from typing import Optional
 from jose import JWTError, jwt
-from fastapi import HTTPException, status
+from fastapi import HTTPException, status, Depends
+from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
+from src.models.database import get_db
+from sqlalchemy.orm import Session
+
+security = HTTPBearer()
 
 def get_password_hash(password:str):
     return bcrypt.hashpw(password.encode('utf-8'),bcrypt.gensalt()).decode('utf-8')
@@ -20,4 +25,18 @@ def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
         expire = datetime.utcnow() + timedelta(minutes=30)
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
-    return encoded_jwt    
+    return encoded_jwt
+def get_current_user(credentials: HTTPAuthorizationCredentials = Depends(security), db: Session = Depends(get_db)):
+    token = credentials.credentials
+    try:
+        payload = jwt.decode(token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM])
+        email: str = payload.get("sub")
+        if email is None:
+            raise HTTPException(status_code = 401, detail="Неверный токен")
+    except JWTError:
+        raise HTTPException(status_code = 401, detail = "Неверный токен")
+    
+    user = db.query(User).filter(User.email == email).first()
+    if user is None:
+        raise HTTPException(status_code = 401, detail="Пользователь не найден")
+    return user
